68e42a30bd60d8ace2849e3ccf673ac8ca7f897c
Darren Jen Kun Tsai <jtsa5115@uni.sydney.edu.au>
2021-04-01 15:51:47 +1100

    minor update

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "alias data = cd /home/user/workspace/data/nuScenes/full" >> ~/.bashrc
-echo "alias centerpoint = cd /home/user/workspace/CenterPoint" >> ~/.bashrc
-
-exec "$@"
-
-/bin/bash
