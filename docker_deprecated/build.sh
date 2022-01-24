9b7e975aa4f0935e9de7f3fe9018361f1b410332
Michael Hannalla <michael.sami.hanna@gmail.com>
2022-01-24 20:59:43 +0200

    changed docker folder name to deprecated to avoid its use

---
+++ docker_deprecated/build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+IMGNAME="centerpoint:ros-melodic-v2"
+
+function usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "    -c,--clean            Clean build."
+    echo "                          Default:$CLEAN"
+    echo "    -h,--help             Display the usage and exit."
+}
+
+OPTS=`getopt --options c:h \
+         --long clean,help \
+         --name "$0" -- "$@"`
+eval set -- "$OPTS"
+
+while true; do
+  case $1 in
+    -c|--clean)
+    echo "Building docker image from scratch."
+      docker build \
+    --build-arg USER_ID=$(id -u) \
+    --build-arg GROUP_ID=$(id -g) \
+    --no-cache \
+    --rm \
+    -t $IMGNAME .
+      exit 0
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    --)
+  if [ ! -z $2 ];
+      then
+        echo "Invalid parameter: $2"
+        exit 1
+      fi
+      break
+      ;;
+    *)
+      echo "Invalid option"
+      exit 1
+      ;;
+  esac
+done
+
+echo "Building docker image from cache."
+docker build \
+--build-arg USER_ID=$(id -u) \
+--build-arg GROUP_ID=$(id -g) \
+--rm \
+-t $IMGNAME .
+
+
---
+++ docker_deprecated/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#VOLUMES="--volume=/tmp/.X11-unix:/tmp/.X11-unix
+#		 --volume=/etc/passwd:/etc/passwd 
+#		 --volume=${PWD}/..:/workspace/CenterPoint
+#		 --volume=/media/darren/Samsung_T5/datasets/nuScenes:/workspace/data/nuScenes"
+
+xhost +local:docker
+
+# env is for the pop up displays
+# ipc=host is required to increased the shared memory size of the docker container for pytorch dataloader
+docker run \
+-it --rm \
+$VOLUMES \
+--env=NVIDIA_VISIBLE_DEVICES=all \
+--env=NVIDIA_DRIVER_CAPABILITIES=all \
+--env=DISPLAY \
+--env=QT_X11_NO_MITSHM=1 \
+--privileged \
+--net=host \
+--gpus=all \
+--ipc=host \
+--workdir="/workspace" \
+centerpoint:ros-melodic-v2
